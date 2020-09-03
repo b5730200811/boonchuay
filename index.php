a359a12d3dfe604fbd6e83369824fca5d3a57c4f
Sahachai Y <three_d1995@hotmail.co.th>
2020-09-03 09:44:45 +0700

    fixed invalid variable

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $output = response($inputJson);
 
 $count = 0;
 
-$eventType = show_event_type($inputJSON);
+$eventType = show_event_type($inputJson);
 
 $text = $eventType;
 $output = add_text($output,$count++,$text);
