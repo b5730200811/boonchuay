1a12bbb99ff35978d70d46310b05cf1775829231
Sahachai Y <three_d1995@hotmail.co.th>
2020-09-07 18:30:48 +0700

    update tmd api get weather

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include "container.php";
 include "ptt.php";
 
 $accesstoken = getenv('LINE_ACCESS_TOKEN');
+$tmdAccesstoken = getenv('TMD_ACCESS_TOKEN');
 
 
 $inputData = file_get_contents("php://input");
@@ -27,7 +28,7 @@ if ($eventType == "follow")
 }
 else if ($eventType == "message") 
 {
-    // $output = add_text($output,$count++, json_encode($inputJson));
+    $output = add_text($output,$count++, json_encode($inputJson));
     $textInput = $inputJson["events"][0]["message"]["text"];
     if ($textInput == "!oil") {
         $text = getOilPrice();
--- tmd.php
+++ tmd.php
@@ -1,4 +1,31 @@
 <?php
 
+function getWeather($lat,$lon) {
+    $sent = curl_init();
+    $url = "https://data.tmd.go.th/nwpapi/v1/forecast/location/hourly/at";
+    
+	curl_setopt($sent,CURLOPT_URL,$url);
+	curl_setopt($sent,CURLOPT_CUSTOMREQUEST,"GET");
+	
+	$arrayheader = array();
+	$arrayheader[] = "Content-Type: application/json";
+    $arrayheader[] = "Authorization: Bearer {$tmdAccesstoken}";
+    
+    $postData = array(
+        'lat' => $lat,
+        'lon' => $lon,
+        'fields' => 'tc,rh,rain,cond',
+        'duration' => 48
+    );
 
+	curl_setopt($sent,CURLOPT_HTTPHEADER,$arrayheader);
+	curl_setopt($sent,CURLOPT_POSTFIELDS,json_encode($postData));
+	curl_setopt($sent,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($sent, CURLOPT_SSL_VERIFYPEER, false);
+    $content = curl_exec($sent);
+    $contentJson = json_decode($content);
+    print_r($contentJson->WeatherForecasts[0]->forecasts);
+    curl_close($sent);
+}
+getWeather('13','100');
 ?>
