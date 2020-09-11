1d178b64b935eff7c71ad37d1a6351fda8ff3dc1
Sahachai Y <three_d1995@hotmail.co.th>
2020-09-11 18:22:06 +0700

    update

--- tmd.php
+++ tmd.php
@@ -4,13 +4,6 @@ function getWeather($lat,$lon) {
     $sent = curl_init();
     $url = "https://data.tmd.go.th/nwpapi/v1/forecast/location/hourly/at";
     
-	curl_setopt($sent,CURLOPT_URL,$url);
-	curl_setopt($sent,CURLOPT_CUSTOMREQUEST,"GET");
-	
-	$arrayheader = array();
-	$arrayheader[] = "Content-Type: application/json";
-    $arrayheader[] = "Authorization: Bearer {$tmdAccesstoken}";
-    
     $postData = array(
         'lat' => $lat,
         'lon' => $lon,
@@ -18,16 +11,27 @@ function getWeather($lat,$lon) {
         'duration' => 48
     );
 
-    curl_setopt($sent,CURLOPT_HTTPHEADER,$arrayheader);
     $url = $url . '?' . http_build_query($postData);
+
     echo "tmd=>".$tmdAccesstoken."<br/>";
+
+	curl_setopt($sent,CURLOPT_URL,$url);
+	curl_setopt($sent,CURLOPT_CUSTOMREQUEST,"GET");
+	
+	$arrayheader = array();
+	$arrayheader[] = "Content-Type: application/json";
+    $arrayheader[] = "Authorization: Bearer {$tmdAccesstoken}";
+    curl_setopt($sent,CURLOPT_HTTPHEADER,$arrayheader);
+
 	//curl_setopt($sent,CURLOPT_POSTFIELDS,json_encode($postData));
 	curl_setopt($sent,CURLOPT_RETURNTRANSFER,true);
     curl_setopt($sent, CURLOPT_SSL_VERIFYPEER, true);
     $content = curl_exec($sent);
+    echo "<br/><br/>content<br/>";
+    print_r($content);
     $contentJson = json_decode($content);
-    
-    return $contentJson;
     curl_close($sent);
+    return $contentJson;
 }
+print_r(getWeather(13.08,100.91));
 ?>
